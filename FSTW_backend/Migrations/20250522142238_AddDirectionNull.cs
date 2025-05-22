5c9c6247f186325f8742e281a67b2254764cd4ae
s1lenter <sega29600@gmail.com>
2025-05-22 19:24:45 +0500

    Разрешить null для направления стажировки

---
+++ FSTW_backend/Migrations/20250522142238_AddDirectionNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSTW_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDirectionNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Direction",
+                table: "Internship",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Direction",
+                table: "Internship",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
